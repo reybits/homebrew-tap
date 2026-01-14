71fcb662881ff2dc8d6cf0a1d18552dc6eb44512
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-01-14 23:17:36 +0100

    fix(formula): update sha256 for texture-packer v1.3.6 source tarball
    The sha256 checksum was updated to match the new source archive.

--- Formula/texture-packer.rb
+++ Formula/texture-packer.rb
@@ -7,7 +7,7 @@ class TexturePacker < Formula
   desc "Texture Packer efficiently packs images into a texture atlas, reducing memory usage and improving rendering performance for games and applications."
   homepage "https://github.com/reybits/texture-packer"
   url "https://github.com/reybits/texture-packer/archive/refs/tags/v1.3.6.tar.gz"
-  # sha256 "54406af9dda99b30d94f9bf3fe7e21882bb9b14adbad4e8851cc2b628d1ad459" # optional
+  sha256 "e90d8fe1a619e8690ee186e61c8a0af07c9054e119c764d390ae48a4e203083b" # optional
   head "https://github.com/reybits/texture-packer.git", branch: "master"
   #   tag:      "v3.2.4",
   #   revision: ""
