4adbd62a3ef03321b89e21fc4a894b49dd44f506
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-07-26 14:32:55 +0200

    chore(formula): bump texture-packer to v1.6.0
    Also fix the test block: it asserted exit 255 for a no-arg run, but
    texpacker prints help and exits 0.

--- Formula/texture-packer.rb
+++ Formula/texture-packer.rb
@@ -6,8 +6,8 @@
 class TexturePacker < Formula
   desc "CLI tool that packs multiple images into optimized texture atlases"
   homepage "https://github.com/reybits/texture-packer"
-  url "https://github.com/reybits/texture-packer/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "c547fed0462c6da0d3a1ef82268336ef81c700d677bf76fa9895b2e02ff000da"
+  url "https://github.com/reybits/texture-packer/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "ad10f7f73134b9a2c6c302f3a135b775812dfdff87c20f876837228a56165f7d"
   license "MIT"
   head "https://github.com/reybits/texture-packer.git", branch: "master"
 
@@ -19,6 +19,6 @@ class TexturePacker < Formula
   end
 
   test do
-    assert_match "Texture Packer", shell_output("#{bin}/texpacker 2>&1", 255)
+    assert_match "Texture Packer", shell_output("#{bin}/texpacker 2>&1")
   end
 end
